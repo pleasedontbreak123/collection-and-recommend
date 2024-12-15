35a420786a32706de7f39cc58be9c1aa788c83d0
pleaserun <nabeiyq@126.com>
2024-12-15 22:57:48 +0800

    初始化提交

---
+++ src/main/java/org/example/collectionandrecommend/CollectionAndRecommendApplication.java
@@ -0,0 +1,13 @@
+package org.example.collectionandrecommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionAndRecommendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionAndRecommendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/collectionandrecommend/CollectionAndRecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.collectionandrecommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionAndRecommendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
